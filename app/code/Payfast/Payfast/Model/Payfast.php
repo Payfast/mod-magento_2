ce9ff4b271061832d53ef4efcea6ab87757e2aec
lefu007 <lefu.ntho@payfast.co.za>
2018-06-15 23:32:25 +0200

    Update Payfast.php
    trim values before urlencoding

--- app/code/Payfast/Payfast/Model/Payfast.php
+++ app/code/Payfast/Payfast/Model/Payfast.php
@@ -357,7 +357,7 @@ class Payfast extends \Magento\Payment\Model\Method\AbstractMethod
         {
             if (!empty( $val ))
             {
-                $pfOutput .= $key .'='. urlencode( $val ) .'&';
+                $pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
             }
         }
 
